21c09696161ec88f4c2224c93128763211e7fb44
Vlad Stoica <vlad.stoick@yahoo.co.uk>
2015-08-18 21:18:50 +0100

    Initial Commit

---
+++ BLE Stoica/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BLE Stoica
+//
+//  Created by Vlad Stoica on 18/08/15.
+//  Copyright © 2015 Vlad Stoica. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ BLE StoicaTests/BLE_StoicaTests.swift
@@ -0,0 +1,36 @@
+//
+//  BLE_StoicaTests.swift
+//  BLE StoicaTests
+//
+//  Created by Vlad Stoica on 18/08/15.
+//  Copyright © 2015 Vlad Stoica. All rights reserved.
+//
+
+import XCTest
+@testable import BLE_Stoica
+
+class BLE_StoicaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
